0ed1aa08c8d5a8933383151d653277d4386cafc0
rpadliwinata <rpadliwinata@student.telkomuniversity.ac.id>
2022-03-19 10:59:58 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+
+namespace Mod4
+{
+    class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T data)
+        {
+            storedData.Add(data);
+            inputDates.Add(DateTime.Now);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine("Data " + (i+1) + " berisi: " + storedData[i].ToString() +  ", yang disimpan pada waktu " + inputDates[i]);
+            }
+        }
+    }
+}
+
